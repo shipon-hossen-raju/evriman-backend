315ecb5c1a7284a2e1f8740655b715d71f25af08
Shipon Hossen Raju <msshipon234@gmail.com>
2025-06-01 16:00:59 +0600

    partner dashboard complete

--- src/app/modules/partner/partner.service.ts
+++ src/app/modules/partner/partner.service.ts
@@ -33,39 +33,92 @@ const getPartnerProfileIntoDb = async (id: string) => {
     },
   });
 
+  // Partner code
   const partnerCode = await prisma.partnerCode.findUnique({
     where: {
-      userId: userData?.id
+      userId: userData?.id,
     },
     select: {
-      partnerCode: true
-    }
+      partnerCode: true,
+    },
   });
 
+  // user links
   const usersLinked = await prisma.user.findMany({
     where: {
-      referralCodeUsed: partnerCode?.partnerCode
+      referralCodeUsed: partnerCode?.partnerCode,
     },
     select: {
       id: true,
       fullName: true,
       email: true,
       userImage: true,
-    }
+      createdAt: true
+    },
   });
 
   // user refer linked count
   const usersLinkedCount = await prisma.user.count({
     where: {
-      referralCodeUsed: partnerCode?.partnerCode
-    }
+      referralCodeUsed: partnerCode?.partnerCode,
+    },
   });
+
+  // my wallet & calculate commission
+  const myWallet = await prisma.payment.findMany({
+    where: {
+      userUsedReferCode: partnerCode?.partnerCode,
+      status: "COMPLETED",
+    },
+    select: {
+      commissionAmount: true,
+    },
+  });
+  const calculateCommission = myWallet.reduce((total, payment) => {
+    // Assuming each payment object has a 'commission' field
+    return total + (payment.commissionAmount || 0);
+  }, 0);
+
+  // find active plans
+  const findUserActivePlanCount = await prisma.payment.count({
+    where: {
+      user: {
+        referralCodeUsed: partnerCode?.partnerCode,
+      },
+      status: "COMPLETED",
+    },
+  });
+
+  // year signup
+  const currentYear = new Date().getFullYear();
+  const yearSignUp = usersLinked.filter(user => {
+    const createdAtYear = new Date(user.createdAt).getFullYear();
+    return createdAtYear === currentYear;
+  }).length;
+
+  // remaining date to next payout (always Dec 1st)
+  const today = new Date();
+  const currentYearDecFirst = new Date(today.getFullYear(), 11, 1); // December is month 11 (0-indexed)
+  let nextPayoutDate: Date;
+
+  if (today < currentYearDecFirst) {
+    nextPayoutDate = currentYearDecFirst;
+  } else {
+    nextPayoutDate = new Date(today.getFullYear() + 1, 11, 1);
+  }
+
+  const msInDay = 1000 * 60 * 60 * 24;
+  const daysToPayout = Math.ceil((nextPayoutDate.getTime() - today.getTime()) / msInDay);
 
   const result = {
     userData,
     partnerCode,
     usersLinked,
     usersLinkedCount,
+    myWallet: calculateCommission,
+    activePlanCount: findUserActivePlanCount,
+    yearSignUp,
+    daysToPayout,
   };
 
   return result;
